e15ed50a22901f92bb8d17ee7cd6eff896dc1cb8
impicciche17051987 <giuseppe.impicciche87@gmail.com>
2019-02-25 14:44:49 +0100

    Exercice3 completed

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,23 @@
 <?php
 
 $input;
-$winner;
+$winner="";
+
+$attemptA=0;
+$attemptB=0;
+
+
+foreach ($input as $combination)
+{
+    if($combination[0]>$combination[1])
+        $attemptA++;
+    if($combination[0]<$combination[1])
+        $attemptB++;
+
+
+}
+
+$winner = ($attemptA>$attemptB)?'A':'B';
+
+echo $winner;
+
