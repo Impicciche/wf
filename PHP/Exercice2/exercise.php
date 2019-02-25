86396ab4bdd522f465feb7041fd3abb705ea54d1
impicciche17051987 <giuseppe.impicciche87@gmail.com>
2019-02-25 12:23:06 +0100

    Second exercices modified after correction

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -3,3 +3,9 @@ $password;
 $salt;
 
 $saltedPassword = '';
+
+$middlePassword = floor(strlen($password)/2);
+$saltedPassword = substr($password,0,$middlePassword) . $salt . substr($password,$middlePassword, strlen($password));
+
+//$saltedPassword = substr($password,0,floor(strlen($password)/2)) . $salt . substr($password,ceil(strlen($password)/2), strlen($password));
+echo $saltedPassword;
