8f8c59040a9a8216972070618df9b670c3365f41
impicciche17051987 <giuseppe.impicciche87@gmail.com>
2019-02-26 09:51:17 +0100

    Exercice4 completed

--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,9 +1,22 @@
 <?php
-function getMondaysOfMonth(int $year, int $month)
-{
-    $date = new DateTime("all monday of $year-$month");
 
+
+function getAllMondaysOfMonth(int $year, int $month) : array 
+{
+    $selectedDate = new DateTime($year . "-" . $month . "-" . "01");
+
+    $dateFirstMonday = new DateTime("first monday of " . $selectedDate->format("Y-m"));
+
+    $dateLastMonday = new DateTime("last monday of " . $selectedDate->format("Y-m"));
+
+    $mondaysArray = [];
+
+    for($currentDate = $dateFirstMonday;$currentDate<=$dateLastMonday;$currentDate->modify("+1 week"))
+        array_push($mondaysArray,$currentDate->format("l j, M Y"));
+
+    return $mondaysArray;
 
 
 }
+var_dump(getAllMondaysOfMonth(2019,4))
 ?>
